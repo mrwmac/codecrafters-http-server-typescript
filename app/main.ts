f10714e7c80edaab464fb67e8627dabc358dd22c
mel <macmely@mail.com>
2024-07-09 21:28:00 +0100

    pass test for second stage

--- app/main.ts
+++ app/main.ts
@@ -5,9 +5,12 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
   socket.on("close", () => {
     socket.end();
   });
 });
+
+console.log('testing');
 
 server.listen(4221, "localhost");
