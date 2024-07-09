e6a6c39c7d4dd0e729ee187592a3942cc59c86c5
mel <macmely@mail.com>
2024-07-09 23:26:59 +0100

    just a test

--- app/main.ts
+++ app/main.ts
@@ -7,7 +7,7 @@ console.log("Logs from your program will appear here!");
 const server = net.createServer((socket) => {
 
     socket.on("data", (data) => {        
-        console.log(data.toString());
+        console.log(data.toString().split('\r\n'));
 
         // socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
     });
